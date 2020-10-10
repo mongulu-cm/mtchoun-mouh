144e043681bc55c71f3fcefa2224b53236aa5694
EC2 Default User <ec2-user@ip-172-31-14-156.ec2.internal>
2020-10-10 22:28:25 +0000

    Updated mail format

--- environment/bangando/environment/notify.py
+++ environment/bangando/environment/notify.py
@@ -12,29 +12,37 @@ def Scan_Users(UserName,Table_Users):
     )
     return response['Items']
 
-def amazone_ses_mail(RECIPIENT):
-    SENDER = "Sender Name <tagnefabiola97@gmail.com>"
+def amazone_ses_mail(NAME,RECIPIENT,URL_IMAGE):
+    
+    NAME=NAME.upper()
+    SENDER = "Mongulu Collective <tagnefabiola97@gmail.com>"
     AWS_REGION = "us-east-1"
-    SUBJECT = "Passeport disponible"
+    SUBJECT = "Ton passeport est disponible"
+    
+    url="https://www.consulacam-marseille.fr/images//communique-22092020-C.jpg"
     BODY_TEXT = ("Votre passeport est sortie  (Python)\r\n"
              "This email was sent with Amazon SES using the "
              "AWS SDK for Python (Boto)."
             )
     BODY_HTML = """<html>
-          <head>Bonjour, </head>
+          <head> <p style="font-size:16px">C Comment {NAME} ?</p></head>
                 <body>
                   
                   <p> 
                     
-                   Votre passeport  est disponible au consulat de Marseille.<br>
-                   N'oubliez pas de vous munir de votre reçu de dépot. <br>
+                   Nous sommes heureux de t'annoncer de que ton <strong>passeport  est disponible au consulat de Marseille</strong> &#128578; .<br>
+                   
+                   Tu peux dès à présent t'y rendre <strong>muni de votre reçu de dépot et votre ancien passeport.</strong> <br>
                    Retrait  des documents etablis du lundi au vendredi de 15H30 à 16H00 (selon l'affluence).<br> <br> <br>
-                   Cordialement, 
+                   
+                   Tu veux voir pour croire ? <br>
+                   On a prévu ça  &#128521;, suis juste ce lien: :  {URL_IMAGE} .<br>
+                   <p style="font-size:16px">On fait comme ça,</p>
                    
                 </p>
                 </body>
                 </html>
-                            """         
+                            """.format(URL_IMAGE=URL_IMAGE, NAME=NAME)     
                             
     CHARSET = "UTF-8"
     client = boto3.client('ses',region_name=AWS_REGION)
@@ -89,9 +97,9 @@ for i in Index_Register:
     print(i)
     Name=i[0]
     Email=i[1]
-    Scan_reponse=Scan_Users(Name,Table_Users)  
+    Scan_reponse=Scan_Users(Name,Table_Users)
     if len(Scan_reponse)==0:
       print(" votre passeport n'est pas sorti")
     else:
-        amazone_ses_mail(Email)
+        amazone_ses_mail(Name,Email,Scan_reponse[0]["URLImage"])
         Delete_Backup(Name)
