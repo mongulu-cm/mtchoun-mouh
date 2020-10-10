211db997051b6834929592b8ba4b20ecb351a0ca
fabiolatagne97 <fabiola-corinne.tagne-tchana@cpe.fr>
2020-10-10 22:28:25 +0000

    Updated mail format

--- environment/bangando/environment/demo.py
+++ environment/bangando/environment/demo.py
@@ -1,11 +1,12 @@
 
 
 from notify import Scan_Users
+from sys import argv
 
 Table_Users="Demo_Users"
 
 if __name__ == "__main__":
-    Name = "METANGMO TSOBZE BILL"
+    Name = argv[1]
     Scan_reponse=Scan_Users(Name.lower(),Table_Users)
     print(Scan_reponse[0]["URLImage"])
 
