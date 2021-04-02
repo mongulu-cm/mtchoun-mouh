7195d176d66c9f1fc8e2ce0e90af3155cd871e2d
Bill Metangmo <25366207+billmetangmo@users.noreply.github.com>
2021-04-02 23:25:23 +0200

    removed bucket redirection
    the redirect bucket tfstate is moved on official website tfstate project.

--- main.tf
+++ main.tf
@@ -59,43 +59,6 @@ EOF
   }
 }
 
-resource "aws_s3_bucket" "redirect" {
-  bucket = var.redirect_bucket_name
-  acl = "public-read"
-
-  tags  = {
-    Name = "Website"
-  }
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT","POST","GET"]
-    allowed_origins = ["*"]
-  }
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadForGetBucketObjects",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${var.redirect_bucket_name}/*"
-    }
-  ]
-}
-EOF
-
-  website {
-    redirect_all_requests_to = var.website_bucket_name
-  }
-
-}
-
 resource "aws_dynamodb_table" "Users" {
   name           = var.table_user
   billing_mode   = "PROVISIONED"
