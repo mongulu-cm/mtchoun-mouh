91d47cff34c38747d76b52fad71f0d41c1dbb5ab
Bill Metangmo <25366207+billmetangmo@users.noreply.github.com>
2021-04-02 23:26:47 +0200

    migrated from local to remote state

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.8.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-
 resource "aws_s3_bucket" "images" {
   bucket=var.images_bucket_name
 
