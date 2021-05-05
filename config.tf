ac2919db36e312c7ed10431cef35ebc84ff7b713
Bill Metangmo <25366207+billmetangmo@users.noreply.github.com>
2021-05-05 18:23:48 +0200

    decrease dynamoDB r/w units from 5 to 1 per table

--- config.tf
+++ config.tf
@@ -37,8 +37,8 @@ resource "aws_dynamodb_table" "terraform-locks" {
     name         = "terraform-locks-mtchoun-mouh"
     billing_mode = "PROVISIONED"
     hash_key     = "LockID"
-    read_capacity  = 5
-    write_capacity = 5
+    read_capacity  = 1
+    write_capacity = 1
 
     attribute {
         name = "LockID"
