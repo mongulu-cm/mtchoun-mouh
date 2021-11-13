a10b23129fce634d804e20c287347b2ed9c41313
Bill Metangmo <25366207+billmetangmo@users.noreply.github.com>
2021-11-13 21:38:09 +0100

    Removed terraform-state bucket
    When using another workspace , terraform failed trying to create the same bucket.

--- config.tf
+++ config.tf
@@ -11,28 +11,6 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-state-mongulu"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-
-
-  versioning {
-    enabled = true
-  }
-
-  //    server_side_encryption_configuration {
-  //        rule {
-  //            apply_server_side_encryption_by_default { aws_s3_bucket.terraform-state aws_dynamodb_table.terraform-locks
-  //                sse_algorithm = "AES256"
-  //            }
-  //        }
-  //    }
-}
-
 terraform {
   required_version = ">= 0.15"
 
