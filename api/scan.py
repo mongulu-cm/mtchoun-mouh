613b375c47c2965373f71d7f1705efa75c19b0ac
billmetangmo <25366207+billmetangmo@users.noreply.github.com>
2022-04-02 23:12:33 +0200

    Unit test -> extract and notify
    updated CI test with unitests
    
    Co-authored-by: fabiolatagne97 <fabiola-corinne.tagne-tchana@cpe.fr>

--- api/scan.py
+++ api/scan.py
@@ -1,19 +1,24 @@
 import sys
-sys.path.insert(0, './package')
+
+sys.path.insert(0, "./package")
 import requests
 import urllib.request
 import boto3
-from bs4 import BeautifulSoup  as soup
+from bs4 import BeautifulSoup as soup
 from boto3.dynamodb.conditions import Attr
 import os
-from config import bucket_name, Table_Links, passport_page,maintainer_mail
+from config import passport_page
 from notify import amazone_ses_mail
+
+bucket_name = os.environ["BUCKET_NAME"]
+Table_Links = os.environ["LINKS_TABLE"]
+maintainer_mail = os.environ["MAINTAINER_MAIL"]
 
 
 def S3_bucket_pictures(Picture_image, bucket_name):
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource("s3")
     key_name = Picture_image.split("/tmp/")[1]
-    s3.Object(bucket_name, key_name).put(Body=open(Picture_image, 'rb'))
+    s3.Object(bucket_name, key_name).put(Body=open(Picture_image, "rb"))
     os.remove(Picture_image)
 
 
@@ -21,7 +26,7 @@ def dowload_image(url):
     ###  this function allows you to download the images  by providing the parameter url
     name = url.split("/")[-1]
     real_image = f"/tmp/{str(name)}"  # image in jpg version ( only /tmp is writable in aws lambda)
-    urllib.request.urlretrieve(url,real_image )
+    urllib.request.urlretrieve(url, real_image)
     return real_image
 
 
@@ -44,28 +49,24 @@ def filter(code_source_html):
 
 
 def insert_link(link_image):
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(Table_Links)
-    table.put_item(
-        Item={
-            'link': link_image
-        }
-    )
+    table.put_item(Item={"link": link_image})
 
 
 def Scan_Link(link_image):
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(Table_Links)
-    response = table.scan(
-        FilterExpression=Attr('link').eq(link_image)
-    )
-    return response['Items']
+    response = table.scan(FilterExpression=Attr("link").eq(link_image))
+    return response["Items"]
 
 
 def scan_consulate_passport_page():
     code_source_html = get_source_code(passport_page)
     tags = filter(code_source_html)
-    srcs = [x.get("src") for x in tags if "communique" in x.get("src")]  ## we select link with word communiqué
+    srcs = [
+        x.get("src") for x in tags if "communique" in x.get("src")
+    ]  ## we select link with word communiqué
     notify_maintainer = False
 
     for src in srcs:
