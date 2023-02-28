bd186e5bfda6c6a0a0ff2b208d2e5870e9104b37
tsafacjo <jordanetsafack@yahoo.fr>
2023-02-28 20:25:56 +0100

    Update test_liveness_mtchoun-mouh.py

--- api/test_liveness_mtchoun-mouh.py
+++ api/test_liveness_mtchoun-mouh.py
@@ -33,9 +33,10 @@ class TestLiveness():
     # 3 | click | id=name-input |  | 
     self.driver.find_element(By.ID, "name-input").click()
     # 4 | type | id=name-input | JORDANE TSAFACK  | 
-    self.driver.find_element(By.ID, "name-input").send_keys("JORDANE TSAFACK ")
+    self.driver.find_element(By.ID, "name-input").send_keys("MONGULU Liveness")
     # 5 | type | id=email-input | jordanetsafack@yahoo.fr | 
-    self.driver.find_element(By.ID, "email-input").send_keys("jordanetsafack@yahoo.fr")
+    self.driver.find_element(By.ID, "email-input").send_keys("hsk6n.mtchoun-mouh.mongulu-cm.hsk6n@inbox.testmail.app
+")
     # 6 | click | css=.btn-primary |  | 
     self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
     # 7 | click | css=.col-4 |  | 
