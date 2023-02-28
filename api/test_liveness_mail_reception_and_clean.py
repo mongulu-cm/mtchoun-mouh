5d5eb39617020364dba182db304d3c611a13ba9b
tsafacjo <jordanetsafack@yahoo.fr>
2023-02-28 22:44:04 +0100

    Update test_liveness_mail_reception_and_clean.py

--- api/test_liveness_mail_reception_and_clean.py
+++ api/test_liveness_mail_reception_and_clean.py
@@ -15,6 +15,7 @@ class TestLivenessNonUi():
     print("Current date:",datetime.utcnow())
     date= datetime.utcnow()+timedelta( minutes = -3) - datetime(1970, 1, 1)
     milliseconds = round(date.total_seconds()*1000)
+    
     url = "https://api.testmail.app/api/json?apikey=89d25786-0bbb-465a-b1d7-bc0a1d90e05b&namespace=hsk6n&pretty=true&timestamp_from={}".format(milliseconds)
     
     # A GET request to the API
