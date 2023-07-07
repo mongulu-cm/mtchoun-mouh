7860965db200e5120b03ddc6f2357692734d1b2c
billmetangmo <25366207+billmetangmo@users.noreply.github.com>
2023-07-07 07:32:56 +0000

    fix: python unittests and e2e

--- infra/main.tf
+++ infra/main.tf
@@ -265,12 +265,13 @@ resource "local_file" "index_page" {
   filename = "../html/index.html"
 }
 
+// Terraform cloud have the file but the CI no so we upload it from terraform cloud 
 resource "aws_s3_bucket_object" "example_file" {
   bucket = aws_s3_bucket.website.id
   key    = "index.html"
   source = "../html/index.html"
   content_type = "text/html"
-  
+
   depends_on = [ local_file.index_page ]
 }
 
