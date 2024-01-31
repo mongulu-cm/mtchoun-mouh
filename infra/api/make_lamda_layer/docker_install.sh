478de9a7f760eecdd4485a38f6f178a0b20fad05
billmetangmo <25366207+billmetangmo@users.noreply.github.com>
2024-01-31 22:30:58 +0000

    chore: clean unused files

--- infra/api/make_lamda_layer/docker_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-virtualenv --python=/usr/bin/python3.8 python #Python is the name of de layer
-soure python/bin/active
-pip install -r requirements.txt -t python/lib/python3.8/site-pakages
-
-zip -r9 python.zip python
