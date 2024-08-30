2b8d48a319fed9ff3f3047d78760cb4220eb11bc
billmetangmo <25366207+billmetangmo@users.noreply.github.com>
2024-08-30 01:17:38 +0000

    fix: make lambda layer

--- infra/api/test_liveness_mtchoun-mouh.py
+++ infra/api/test_liveness_mtchoun-mouh.py
@@ -14,7 +14,7 @@ class TestLiveness():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    self.driver = webdriver.Chrome(chrome_options=chrome_options)
+    self.driver = webdriver.Chrome(options=chrome_options)
     self.vars = {}
   
   def teardown_method(self, method):
